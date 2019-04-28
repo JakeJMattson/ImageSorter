380e5481db5035cd5c9d9f951580a482ef395682
Jake Mattson <JakeJMattson@gmail.com>
2019-04-27 23:55:16 -0500

    Add with statement

--- src/main/kotlin/io/github/jakejmattson/imagesorter/ImageSorter.kt
+++ src/main/kotlin/io/github/jakejmattson/imagesorter/ImageSorter.kt
@@ -9,16 +9,18 @@ fun main() {
 	val output = File("output.png")
 	val image = ImageIO.read(input)!!
 
-	val pixels = image.extractPixels().also { it.sort() }
+	with(image) {
+		val pixels = extractPixels().also { it.sort() }
 
-	val newImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
-	newImage.setRGB(0, 0, image.width, image.height, pixels, 0, image.width)
+		val newImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+		newImage.setRGB(0, 0, width, height, pixels, 0, width)
 
-	ImageIO.write(newImage, output.extension, output)
+		ImageIO.write(newImage, output.extension, output)
+	}
 }
 
-private fun BufferedImage.extractPixels(): IntArray {
-	val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-	image.graphics.drawImage(this, 0, 0, null)
-	return (image.raster.dataBuffer as DataBufferInt).data
-}
+private fun BufferedImage.extractPixels() =
+	with (BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)) {
+		graphics.drawImage(this, 0, 0, null)
+		(raster.dataBuffer as DataBufferInt).data
+	}
