f97b7a645c17431f8a3cc16759067df826534a84
Jake Mattson <JakeJMattson@gmail.com>
2019-07-13 23:52:29 -0500

    Use apply instead of with

--- src/main/kotlin/io/github/jakejmattson/imagesorter/ImageSorter.kt
+++ src/main/kotlin/io/github/jakejmattson/imagesorter/ImageSorter.kt
@@ -9,12 +9,10 @@ fun main() {
 	val output = File("output.png")
 	val image = ImageIO.read(input)!!
 
-	val newImage =
-		with (BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)) {
+	val newImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB).apply {
 			graphics.drawImage(image, 0, 0, null)
 			val pixels = (raster.dataBuffer as DataBufferInt).data.sorted().toIntArray()
 			setRGB(0, 0, width, height, pixels, 0, width)
-			this
 		}
 
 	ImageIO.write(newImage, output.extension, output)
